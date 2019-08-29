69753016f035eb0e2ccd6be6fccbac15c5da0b2f
alexander-agysov <alexander.agysov@itechcraft.com>
2019-08-29 21:49:28 +0300

    fixed

--- todo-list.js
+++ todo-list.js
@@ -3,132 +3,166 @@
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-  const listBox = document.querySelector("#list-box");
-  let storageArr = JSON.parse(localStorage.getItem("storageArr"));
-  let listItemArr = [];
-  let nameField = document.querySelector("#item-name-field");
-  let descriptionField = document.querySelector("#item-description-field");
-  let todoForm = document.querySelector("#todo-form");
+  todoInit();
+}
 
-  todoForm.addEventListener("submit", addItem);
-  listBox.addEventListener("click", changeStatus);
-  document.querySelector("#sort-select").addEventListener("change", sortArr);
-
-  if (storageArr) {
-    storageArr.forEach(function (item) {
-      item.date = new Date(item.date);
-    });
-
-    listItemArr = storageArr;
+class TodoListStorage {
+  static get() {
+    return JSON.parse(localStorage.getItem("todoList")) || {};
   }
 
-  class ListItem {
-    constructor(title, description, status = "Active") {
-      this.id = "_" + Math.random().toString(36).substr(2, 9);
-      this.date = new Date();
-      this.title = title;
-      this.description = description;
-      this.status = status;
+  static set(todoList) {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }
+}
+
+class TodoList {
+  constructor(rootEl, { order = "date", items = [] }) {
+    this.rootEl = rootEl;
+    this.order = order;
+    this.items = items.map(item => new ListItem(item));
+  }
+
+  addTodo(name, description) {
+    if (!name) return;
+    this.items.push(new ListItem({ name, description }));
+    this.setToStorage();
+    this.render();
+  }
+
+  deleteTodo(id) {
+    this.items.splice(this.items.findIndex(item => item.id == id), 1);
+    this.setToStorage();
+    this.render();
+  }
+
+  completeTodo(id) {
+    this.items.find(item => item.id == id).complete();
+    this.setToStorage();
+    this.render();
+  }
+
+  sortList() {
+    if (this.order == "date") {
+      this.items.sort((a, b) => a.date - b.date);
+    }
+
+    if (this.order == "alphabetical") {
+      this.items.sort((a, b) => {
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+      });
     }
   }
 
-  class Ui {
-    formatDate(date) {
-      // date = new Date(date);
-      let dd = date.getDate();
-      if (dd < 10) dd = "0" + dd;
+  setSelectValue(selectValue) {
+    this.order = selectValue;
+    this.sortList();
+    this.setToStorage();
+    this.render();
+  }
 
-      let mm = date.getMonth() + 1;
-      if (mm < 10) mm = "0" + mm;
+  getSelectValue(elem) {
+    elem.value = this.order;
+  }
 
-      let yy = date.getFullYear();
+  formatDate(date) {
+    let dd = date.getDate();
+    if (dd < 10) dd = "0" + dd;
 
-      return `${dd}.${mm}.${yy}`;
-    }
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = "0" + mm;
 
-    clearValue(field1, field2) {
-      field1.value = "";
-      field2.value = "";
-    }
+    let yy = date.getFullYear();
 
-    createItem(obj) {
+    return `${dd}.${mm}.${yy}`;
+  }
+
+  render() {
+    this.rootEl.innerHTML = "";
+    this.items.forEach(item => {
       const row = `<hr/>
-                    <div data-id="${obj.id}" class="row">
-                      <div class="col-3">${this.formatDate(obj.date)}</div>
-                      <div class="col-6">
-                        <h5>${obj.title}</h5>
-                        <div>${obj.description}</div>
-                      </div>
-                      <div class="col-3">
-                        <button class="status-btn btn btn-sm btn-secondary">${obj.status}</button>
-                      </div>
-                    </div>`;
-      listBox.insertAdjacentHTML("beforeEnd", row);
+                  <div data-id="${item.id}" class="row">
+                    <div class="col-2">${this.formatDate(item.date)}</div>
+                    <div class="col">
+                      <h5>${item.name}</h5>
+                      <div>${item.description}</div>
+                    </div>
+                    <div class="col-2">
+                      <button class="status-btn btn btn-sm btn-secondary">${
+        item.status
+        }</button>
+                    </div>
+                    <div class="col-3">
+                      <button class="complete-btn btn btn-sm btn-primary">Complete</button>
+                    </div>
+                  </div>`;
+      this.rootEl.insertAdjacentHTML("beforeEnd", row);
+    });
+  }
+
+  setToStorage = () => {
+    TodoListStorage.set({
+      order: this.order,
+      items: this.items
+    });
+  };
+}
+
+class ListItem {
+  constructor({ id, name, description, date }) {
+    this.id =
+      id ||
+      "_" +
+      Math.random()
+        .toString(36)
+        .substr(2, 9);
+    this.date = date ? new Date(date) : new Date();
+    this.name = name;
+    this.description = description;
+    this.status = "Active";
+    this.completed = false;
+  }
+
+  complete() {
+    this.status = "Complete";
+  }
+}
+
+function todoInit() {
+  const listBox = document.querySelector("#list-box");
+  const todoForm = document.querySelector("#todo-form");
+  const inputSelect = document.querySelector("#sort-select");
+
+  const todoList = new TodoList(listBox, TodoListStorage.get());
+
+  todoList.render();
+  todoList.getSelectValue(inputSelect);
+
+  todoForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    todoList.addTodo(this.name.value, this.description.value);
+    this.reset();
+  });
+
+  listBox.addEventListener("click", function (e) {
+    const completeBtn = e.target.closest(".complete-btn");
+    const statusBtn = e.target.closest(".status-btn");
+
+    if (completeBtn) {
+      todoList.deleteTodo(
+        completeBtn.closest("[data-id]").getAttribute("data-id")
+      );
     }
-  }
 
-  render();
-
-  function addItem() {
-    let nameValue = nameField.value;
-    if (!nameValue) return;
-
-    let itemObj = new ListItem(nameValue, descriptionField.value);
-
-    let ui = new Ui();
-    ui.createItem(itemObj);
-    ui.clearValue(nameField, descriptionField);
-
-    listItemArr.push(itemObj);
-
-    saveToStorage();
-  }
-
-  function changeStatus(e) {
-    let statusBtn = e.target.closest(".status-btn");
-    if (!statusBtn) return;
-
-    statusBtn.classList.toggle("btn-secondary");
-    statusBtn.classList.toggle("btn-success");
-
-    statusBtn.textContent = statusBtn.textContent.toLowerCase() == "active" ? "Completed" : "Active";
-
-    let obj = listItemArr.find(item => item.id == statusBtn.closest("[data-id]").getAttribute("data-id"));
-    // console.log(listItemArr); why status prop the same?
-    obj.status = obj.status.toLowerCase() == "active" ? "Completed" : "Active";
-    // console.log(listItemArr); why status prop the same?
-    saveToStorage();
-  }
-
-
-  function sortArr() {
-    let optionValue = this.options[this.selectedIndex].value;
-
-    if (optionValue == "date") {
-      listItemArr.sort(function (a, b) {
-        return a.date - b.date;
-      });
+    if (statusBtn) {
+      todoList.completeTodo(
+        statusBtn.closest("[data-id]").getAttribute("data-id")
+      );
     }
+  });
 
-    if (optionValue == "alphabetical") {
-      listItemArr.sort(function (a, b) {
-        return a.title - b.title;
-      });
-    }
-    render();
-    saveToStorage();
-  }
-
-  function render() {
-    let ui = new Ui();
-    listBox.innerHTML = "";
-
-    for (let i = 0; i < listItemArr.length; i++) {
-      ui.createItem(listItemArr[i]);
-    }
-  }
-
-  function saveToStorage() {
-    localStorage.setItem("storageArr", JSON.stringify(listItemArr));
-  }
+  inputSelect.addEventListener("change", function (e) {
+    todoList.setSelectValue(this.value);
+  });
 }
